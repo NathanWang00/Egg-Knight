5da1e2058a6c9b2c7e84c1b6b8c3ec26ff3537f2
Nathan Wang <nxwang@ucsc.edu>
2022-03-24 21:56:43 -0700

    Initial slash graphics

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,19 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEngine.Android;
 
 public class GameManager : MonoBehaviour
 {
-    [Header("Debug Section")]
+    [Header("Debug")]
     [SerializeField] protected bool debugOn;
     [SerializeField] protected GameObject debugObject;
-    [SerializeField] protected TMPro.TextMeshProUGUI vectorText;
+    [SerializeField] protected TextMeshProUGUI vectorText;
 
     [Space(10)]
 
-    protected Vector2 touchPos, lastTouch, lastLastTouch;
-    protected float angleChange;
+    [Header("Touch Controls")]
+    [SerializeField] protected float startRadius = 10; // amount of area needed to start the slash
+    protected float initialAngle, trackDelay = 0.0116f, currentDelay = 0, currentAngle;
+    protected Vector2 initialPos, touchPos, touchWorldPoint, lastTouch, currentVelo;
+    protected bool slashOn = false;
+
+    [Space(10)]
+
+    [Header("Graphics")]
+    [SerializeField] protected float minLineChange = 0.1f;
+    protected float linePosition = 0;
+    protected LineRenderer slashLine;
 
     private static GameManager _instance;
     public static GameManager Instance //Singleton Stuff
@@ -34,9 +43,89 @@ public class GameManager : MonoBehaviour
         _instance = this;
     }
 
+    private void Start()
+    {
+        slashLine = GetComponentInChildren<LineRenderer>();
+    }
+
     private void Update()
     {
-        Vector2 lastVelo = Vector2.zero, currentVelo = Vector2.zero;
+        if (Input.GetMouseButtonDown(0))
+        {
+            initialPos = GetTouchPos();
+            initialAngle = 0;
+            lastTouch = touchPos;
+            currentDelay = trackDelay - Time.deltaTime;
+            slashLine.positionCount = 0;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            currentDelay += Time.deltaTime;
+            if (currentDelay >= trackDelay)
+            {
+                GetTouchPos();
+
+                currentDelay -= trackDelay;
+                currentVelo = touchPos - lastTouch;
+                currentAngle = Mathf.Rad2Deg * Mathf.Atan2(currentVelo.y, currentVelo.x);
+
+                if (currentAngle < 0)
+                {
+                    currentAngle += 360;
+                }
+
+                // slash stuff and graphics
+                if (!slashOn)
+                {
+                    if ((touchPos - initialPos).magnitude > startRadius)
+                    {
+                        initialAngle = currentAngle;
+                        slashOn = true;
+                        Debug.Log("on");
+                    }
+                }
+                else
+                {
+                    if (slashLine.positionCount > 0)
+                    {
+                        if (((Vector2)slashLine.GetPosition(slashLine.positionCount - 1) - touchWorldPoint).magnitude > minLineChange)
+                        {
+                            slashLine.positionCount += 1;
+                            slashLine.SetPosition(slashLine.positionCount - 1, new Vector3(touchWorldPoint.x, touchWorldPoint.y, 0));
+                        }
+                    }
+                    else
+                    {
+                        slashLine.positionCount += 1;
+                        slashLine.SetPosition(slashLine.positionCount - 1, new Vector3(touchWorldPoint.x, touchWorldPoint.y, 0));
+                    }
+                }
+
+                // debug stuff
+                if (debugObject != null)
+                {
+                    debugObject.SetActive(debugOn);
+                    if (Input.GetMouseButton(0))
+                    {
+                        vectorText.text = "Touch Vector: " + touchPos + "\n" + "Touch Velo: " + currentVelo + "\n" + "Angle: " + currentAngle;
+                    }
+                }
+
+                // setting last touch to current
+                if (Input.GetMouseButton(0) && lastTouch != touchPos)
+                {
+                    lastTouch = touchPos;
+                }
+            }
+        } 
+        if (Input.GetMouseButtonUp(0))
+        {
+            slashOn = false;
+        }
+    }
+
+    public Vector2 GetTouchPos() // also sets touchPos. If there's no touch, gives the last touchPos
+    {
         if (Input.GetMouseButton(0))
         {
             if (Input.touchCount > 0)
@@ -47,36 +136,8 @@ public class GameManager : MonoBehaviour
             {
                 touchPos = Input.mousePosition;
             }
+            touchWorldPoint = Camera.main.ScreenToWorldPoint(touchPos);
         }
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            angleChange = 0;
-            lastTouch = touchPos;
-            lastLastTouch = touchPos;
-        } 
-        else if (Input.GetMouseButton(0))
-        {
-            if (touchPos - lastTouch != Vector2.zero)
-            {
-                lastVelo = lastTouch - lastLastTouch;
-                currentVelo = touchPos - lastTouch;
-            }
-        }
-
-        if (debugObject != null)
-        {
-            debugObject.SetActive(debugOn);
-            if (Input.GetMouseButton(0))
-            {
-                vectorText.text = "Touch Vector: " + touchPos + "\n" + "Touch Velo: " + currentVelo + "\n" + Vector2.Angle(lastVelo, currentVelo);
-            }
-        }
-
-        if (Input.GetMouseButton(0) && lastTouch != touchPos)
-        {
-            lastLastTouch = lastTouch;
-            lastTouch = touchPos;
-        }
+        return touchPos;
     }
 }
